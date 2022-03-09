ce86de103578557512b1bdf0913cf24baf676ad4
@dank1910 <53305399+dan141910@users.noreply.github.com>
2022-03-09 10:56:51 +0700

    initialization

---
+++ modules/metrics.py
@@ -0,0 +1,139 @@
+#head
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+import math
+
+###########Air Face##############
+class AirMarginProduct(nn.Module):
+    """fine tune Arcface https://arxiv.org/pdf/1907.12256.pdf :
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            s: norm of input feature
+            m: margin
+            cos(pi-(theta + m))
+        """
+    def __init__(self, in_features, out_features, s=64.0, m=0.4):
+        super(AirMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight) #init weight
+        self.eps = 1e-7
+
+    def forward(self, input, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cos_t = F.linear(F.normalize(input), F.normalize(self.weight, dim=1)).clamp(-1. + self.eps, 1. - self.eps)
+        t = torch.arccos(cos_t)
+        phi = math.pi - 2 * t - 2 * self.m
+        not_phi = math.pi - 2 * t
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = torch.zeros_like(cos_t)
+        one_hot.scatter_(1, label.view(-1, 1), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = torch.where(one_hot == 1, phi, not_phi)
+        output *= (self.s / math.pi)
+
+        return output
+
+
+###########ArcFace##############
+class ArcMarginProduct(nn.Module):
+    """Implement of large margin arc distance: :
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            s: norm of input feature
+            m: margin
+            cos(theta + m)
+        """
+    def __init__(self, in_features, out_features, s=64.0, m=0.50):
+        super(ArcMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight) #init weight
+        self.eps = 1e-7
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.th = math.cos(math.pi - m) #threshold
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, input, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cos_t = F.linear(F.normalize(input), F.normalize(self.weight, dim=1))
+        cos_t = cos_t.clamp(-1. + self.eps, 1. - self.eps)
+        sin_t = torch.sqrt(1.0 - torch.pow(cos_t, 2))
+        cos_phi = cos_t * self.cos_m - sin_t * self.sin_m
+        cos_phi = torch.where(cos_t > self.th, cos_phi, cos_t - self.mm)
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = torch.zeros_like(cos_phi)
+        one_hot.scatter_(1, label.view(-1, 1), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = torch.where(one_hot == 1, cos_phi, cos_t)
+        output *= self.s
+
+        return output
+
+
+###########CosFace##############
+class CosMarginProduct(nn.Module):
+    """Implement of large margin cosine distance: :
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        s: norm of input feature
+        m: margin
+        cos(theta) - m
+    """
+
+    def __init__(self, in_features, out_features, s=64.0, m=0.40):
+        super(CosMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, input, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cos_t = F.linear(F.normalize(input), F.normalize(self.weight, dim=1))
+        phi = cos_t - self.m
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = F.one_hot(label, num_classes=self.out_features)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = torch.where(one_hot==1, phi, cos_t)
+        output *= self.s
+        # print(output)
+
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', s=' + str(self.s) \
+               + ', m=' + str(self.m) + ')'
+
+###########MagFace##############
+class MagMarginProduct(nn.Module):
+    pass
+
+###########normal softmax##############
+class NormalFCLayer(nn.Module):
+    def __init__(self, in_features, out_features) -> None:
+        super(NormalFCLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+        
+    def forward(self, input, label):
+        return F.linear(input, F.normalize(self.weight))
---
+++ train.py
@@ -0,0 +1,175 @@
+import os
+import argparse
+import torch
+import yaml
+import tqdm
+import time
+from torch.nn import DataParallel
+from torch.optim import SGD, Adam
+from torch.optim.lr_scheduler import StepLR, MultiStepLR
+from torch.utils.tensorboard import SummaryWriter
+
+from modules.models import MobileFaceNet, Backbone
+from modules.dataloader import get_DataLoader, TrainDataset, ValidDataset
+from modules.metrics import CosMarginProduct, ArcMarginProduct, NormalFCLayer, AirMarginProduct
+from modules.evaluate import evaluate_model
+from modules.focal_loss import FocalLoss
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--c', type=str, default='./configs/res50.yaml', help='config path')
+    parser.add_argument("--n", type=int, default=2, help="the number of workers")
+    return parser.parse_args()
+
+def save_model(model, save_path, name, iter_cnt):
+    save_name = os.path.join(save_path, name + '_' + str(iter_cnt) + '.pth')
+    torch.save(model.state_dict(), save_name)
+    return save_name
+
+def main(cfg, n_workers=2):
+    #setup device
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device('cpu')
+    #setup path
+    save_path = os.path.join(os.getcwd(), "save", cfg['model_name'])
+    ckpt_path = os.path.join(save_path, "ckpt")
+    log_path  = os.path.join(save_path, "log")
+    if not os.path.exists(ckpt_path): os.makedirs(ckpt_path)
+    if not os.path.exists(log_path): os.makedirs(log_path)
+    #train data
+    train_dataset = TrainDataset(data_list_file=cfg['train_data'],
+                       is_training=True,
+                       input_shape=(3, cfg['image_size'], cfg['image_size']))
+    trainloader = get_DataLoader(train_dataset,
+                                   batch_size=cfg['batch_size'],
+                                   shuffle=True,
+                                  num_workers=n_workers)
+    #valid data
+    valid_set = {}
+    for x in cfg['valid_data']:
+        valid_dataset = ValidDataset(data_list_file=cfg['valid_data'][x])
+        valid_set[x] = get_DataLoader(valid_dataset,
+                                batch_size=cfg['batch_size'],
+                                shuffle=True,
+                                num_workers=n_workers)
+
+    #get backbone
+    if cfg['backbone'].lower() == 'resnet50':
+        print("use ir-resnet50")
+        backbone = Backbone(50, 0.5, mode='ir_se')
+    elif cfg['backbone'].lower() == 'resnet100':
+        print("use ir-resnet100")
+        backbone = Backbone(100, 0.5, mode='ir_se')
+    else:
+        print("use mobile FaceNet")
+        backbone = MobileFaceNet(cfg['embd_size'])
+
+    #metrics
+    if cfg['loss'].lower() == 'cosloss':
+        print("use Cos-Loss")
+        partial_fc = CosMarginProduct(in_features=cfg['embd_size'],
+                                out_features=cfg['class_num'],
+                                s=cfg['logits_scale'], m=cfg['logits_margin'])
+    elif cfg['loss'].lower() == 'arcloss':
+        print("use ArcLoss")
+        partial_fc = ArcMarginProduct(in_features=cfg['embd_size'],
+                                out_features=cfg['class_num'],
+                                s=cfg['logits_scale'], m=cfg['logits_margin'])
+    elif cfg['loss'].lower() == 'airloss':
+        print("use L-ArcLoss")
+        partial_fc = AirMarginProduct(in_features=cfg['embd_size'],
+                                out_features=cfg['class_num'],
+                                s=cfg['logits_scale'], m=cfg['logits_margin'])
+    else:
+        print("No Additative Margin")
+        partial_fc = NormalFCLayer(in_features=cfg['embd_size'],
+                                out_features=cfg['class_num'])
+    
+    #data parapell
+    backbone = DataParallel(backbone.to(device))
+    partial_fc = DataParallel(partial_fc.to(device))
+
+    #optimizer
+    if 'optimizer' in cfg.keys() and cfg['optimizer'].lower() == 'adam':
+        optimizer = Adam([{'params': backbone.parameters()}, {'params': partial_fc.parameters()}],
+                                    lr=cfg['base_lr'], weight_decay=cfg['weight_decay'])
+    else:
+        optimizer = SGD([{'params': backbone.parameters()}, {'params': partial_fc.parameters()}],
+                                     lr=cfg['base_lr'], weight_decay=cfg['weight_decay'])
+    #LossFunction+scheduerLR
+    if cfg['criterion'] == 'focal':
+        criterion = FocalLoss(gamma=2)
+    else:
+        criterion = torch.nn.CrossEntropyLoss()
+    
+    
+    scale = int(512 / cfg['batch_size'])
+    lr_steps = [ int(scale * s) for s in cfg['lr_steps']] #epochs
+    scheduler = MultiStepLR(optimizer, milestones=lr_steps, gamma=0.1)
+
+    print(lr_steps)
+    #loop
+    steps_per_epoch = cfg['sample_num'] // cfg['batch_size'] + 1
+    stps = steps_per_epoch if str(cfg["step_per_save"]) == "epoch" else int(cfg['step_per_save']) 
+    max_acc = 0.
+    writer = SummaryWriter(log_path)
+    for e in range(1,cfg['epoch_num']+1):
+        s = time.time()
+        print("Epoch: {}/{} \n-LR: {:.6f} \n-Train...".format(e,cfg['epoch_num'], scheduler.get_last_lr()[0]))
+        backbone.train()
+        total_loss = 0.0
+        num_batchs = 0
+        num_correct = 0.
+        for data in tqdm.tqdm(iter(trainloader)):
+            inputs, label = data
+            inputs = inputs.to(device)
+            label = label.to(device).long()
+
+            embds = backbone(inputs)
+            logits = partial_fc(embds, label)
+            loss = criterion(logits, label)
+            
+            #update metrics
+            total_loss += loss.item()
+            num_batchs += 1
+            indices = torch.max(logits, 1)[1]
+            num_correct += torch.sum(torch.eq(indices, label).view(-1)).item()
+            
+            #update weights
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(backbone.parameters(), 5)
+            optimizer.step()
+            
+            if num_batchs % stps == 0:    # every 1000 mini-batches...
+                # ...log the running loss
+                writer.add_scalar('training loss',
+                            total_loss / num_batchs,
+                            (e-1) * len(trainloader) + num_batchs)
+                writer.add_scalar('learning rate',
+                            scheduler.get_last_lr()[0],
+                            (e-1) * len(trainloader) + num_batchs)
+                writer.add_scalar('training accuracy', 
+                            num_correct / num_batchs, 
+                            (e-1) * len(trainloader) + num_batchs)
+        scheduler.step()         
+        #test
+        backbone.eval()
+        print("-Validate...") 
+        with torch.no_grad():
+            for x in cfg['valid_data']:
+                acc, _, _ = evaluate_model(backbone, valid_set[x], device=device)
+                writer.add_scalar('verification accuracy _ {} dataset'.format(x), acc, e * num_batchs)
+                print('\t--{}\'s accuracy: {:.5f}'.format(x,acc))
+            save_model(backbone,  ckpt_path, cfg['model_name'], e)     
+        print("\t--Train Loss: {:.5f} \n\t--Train accuracy: {:.5f}".format(total_loss / num_batchs, num_correct / cfg['sample_num']))
+        print('\t--total time is {:.3f}'.format(time.time()-s))
+    writer.close()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    with open(args.c, 'r') as file:
+        config = yaml.load(file, Loader=yaml.Loader)
+    print(config)
+    main(config ,n_workers=args.n)
